1d4c4f9cdf9f6aa586d6ce5f437224dd7d28eff5
Tyler Shimko <tshimko@trident.bio>
2022-02-26 09:00:34 -0800

    Set prefer_coodgen option of RDKit to true

--- src/utils/rdkit.ts
+++ src/utils/rdkit.ts
@@ -21,6 +21,12 @@ const initRDKit = (() => {
             // we have to declare an interface for it
             rdKitLoadingPromise = new Promise((resolve, reject) => {
                 (window as any).initRDKitModule().then((RDKit: any) => {
+
+                    // Set local RDKit distribution to prefer coordgen
+                    // coordinate system for better rendering of macrocycles
+                    RDKit.prefer_coordgen(true);
+
+                    // Set window property
                     (window as any).RDKit = RDKit;
                     resolve(RDKit);
                 }).catch((__: Error) => {
