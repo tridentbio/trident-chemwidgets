71e6cd6dcc293bd18717e2efa5f8aff76ec1a255
Gilberto Barbosa <jose.gilberto@shawandpartners.com>
2022-05-30 09:27:43 -0300

    feature(scatter): add hue and date support

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def create_local_assets_provider():
     files for notebooks and jupyter lab. 
     """
     CONFIG_DIR = jupyter_config_dir()
-    ASSETS_DIR = os.path.join(expanduser('~'), '.trident_chemwidgets/')
+    ASSETS_DIR = os.path.join(expanduser('~'), '.trident_chemwidgets/assets')
 
     # Copy assets to the user .trident-chemwidgets
     shutil.copytree(
@@ -39,7 +39,7 @@ def create_local_assets_provider():
     )
 
     # Convert the path for unix path convertion since
-    # the windows use the backslashes 
+    # the windows use the backslashes
     static_path = ASSETS_DIR.replace('\\', '/')
 
     # Create a jupyter lab config file
@@ -49,6 +49,7 @@ def create_local_assets_provider():
     # Create a jupyter notebook config file
     with open(os.path.join(CONFIG_DIR, 'jupyter_notebook_config.py'), 'w') as file:
         file.write(f"c.NotebookApp.extra_static_paths = ['{static_path}']")
+
 
 create_local_assets_provider()
 
@@ -71,14 +72,16 @@ package_data_spec = {
 }
 
 data_files_spec = [
-    ('share/jupyter/nbextensions/trident_chemwidgets', 'trident_chemwidgets/nbextension', '**'),
-    ('share/jupyter/labextensions/trident-chemwidgets', 'trident_chemwidgets/labextension', '**'),
+    ('share/jupyter/nbextensions/trident_chemwidgets',
+     'trident_chemwidgets/nbextension', '**'),
+    ('share/jupyter/labextensions/trident-chemwidgets',
+     'trident_chemwidgets/labextension', '**'),
     ('share/jupyter/labextensions/trident-chemwidgets', '.', 'install.json'),
     ('etc/jupyter/nbconfig/notebook.d', '.', 'trident_chemwidgets.json'),
 ]
 
 cmdclass = create_cmdclass('jsdeps', package_data_spec=package_data_spec,
-    data_files_spec=data_files_spec)
+                           data_files_spec=data_files_spec)
 npm_install = combine_commands(
     install_npm(HERE, build_cmd='build:prod'),
     ensure_targets(jstargets),
@@ -86,19 +89,19 @@ npm_install = combine_commands(
 cmdclass['jsdeps'] = skip_if_exists(jstargets, npm_install)
 
 setup_args = dict(
-    name            = name,
-    description     = 'Jupyter widgets for working with chemical datasets.',
-    version         = version,
-    scripts         = glob(pjoin('scripts', '*')),
-    cmdclass        = cmdclass,
-    packages        = find_packages(),
-    author          = 'Trident Bioscience, Inc.',
-    author_email    = 'info@trident.bio',
-    url             = 'https://github.com/tridentbio/trident-chemwidgets',
-    license         = 'BSD',
-    platforms       = "Linux, Mac OS X, Windows",
-    keywords        = ['Jupyter', 'Widgets', 'IPython'],
-    classifiers     = [
+    name=name,
+    description='Jupyter widgets for working with chemical datasets.',
+    version=version,
+    scripts=glob(pjoin('scripts', '*')),
+    cmdclass=cmdclass,
+    packages=find_packages(),
+    author='Trident Bioscience, Inc.',
+    author_email='info@trident.bio',
+    url='https://github.com/tridentbio/trident-chemwidgets',
+    license='BSD',
+    platforms="Linux, Mac OS X, Windows",
+    keywords=['Jupyter', 'Widgets', 'IPython'],
+    classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
@@ -110,13 +113,13 @@ setup_args = dict(
         'Programming Language :: Python :: 3.7',
         'Framework :: Jupyter',
     ],
-    include_package_data = True,
+    include_package_data=True,
     python_requires=">=3.6",
-    install_requires = [
+    install_requires=[
         'ipywidgets>=7.0.0',
         'pandas>=1.0.0'
     ],
-    extras_require = {
+    extras_require={
         'test': [
             'pytest>=4.6',
             'pytest-cov',
@@ -138,7 +141,7 @@ setup_args = dict(
             'furo',
         ],
     },
-    entry_points = {
+    entry_points={
     },
 )
 
--- src/widgets/scatterPlot/ScatterWidget.tsx
+++ src/widgets/scatterPlot/ScatterWidget.tsx
@@ -11,8 +11,15 @@ const ScatterWidget = (props: WidgetProps): JSX.Element => {
     return (
         <Scatter data={props.model.get('data')}
                  xLabel={props.model.get('x_label')}
+                 xIsDate={props.model.get('x_is_date')}
+                 xDateFormat={props.model.get('x_format_date_string')}
                  yLabel={props.model.get('y_label')}
+                 yIsDate={props.model.get('y_is_date')}
+                 yDateFormat={props.model.get('y_format_date_string')}
                  hueLabel={props.model.get('hue_label')}
+                 hueType={props.model.get('hue_type')}
+                 hueMin={props.model.get('hue_min')}
+                 hueMax={props.model.get('hue_max')}
                  onChange={setSavedSelected} />
     );
 }
